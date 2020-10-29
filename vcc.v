96c55817faf34266ce055576f2aa20da69fe9cc8
natsuokawai <kawai.natsuo@gmail.com>
2020-10-29 22:56:55 +0900

    Refactor: Add operators: +, -

--- vcc.v
+++ vcc.v
@@ -6,43 +6,53 @@ fn main() {
     return
   }
 
-  input := os.args[1]
+  mut input_str := os.args[1]
+  mut num := 0
+  mut op  := ''
 
   println('.global main')
   println('main:')
 
-  mut i   := 0
-  mut num := ''
-  for input[i].is_digit() {
-    num += input[i]
-    i++
-  }
-  println('  mov \$${num.int()}, %rax')
+  num, input_str = consume_number(input_str)
+  println('  mov \$$num, %rax')
 
-  for i < input.len {
-    num = ''
-    match input[i] {
+  for input_str.len > 0 {
+    op, input_str = consume_operator(input_str) or { exit(1) }
+    match op {
       '+' {
-        i++
-        for input[i].is_digit() {
-          num += input[i]
-          i++
-        }
-        println('  add \$num, %rax')
+        num, input_str = consume_number(input_str)
+        println('  add \$$num, %rax')
       }
       '-' {
-        i++
-        for input[i].is_digit() {
-          num += input[i]
-          i++
-        }
-        println('  sub \$num, %rax')
+        num, input_str = consume_number(input_str)
+        println('  sub \$$num, %rax')
       }
-      else { eprintln('unexpected character: ${input[i]') }
+      else {
+        eprintln('unexpected character: $op')
+        return
+      }
     }
   }
   
   println('  ret')
   return 
+}
+
+fn consume_number(s string) (int, string) {
+  mut num := ''
+  mut i   := 0
+  for i < s.len && s[i].is_digit() {
+    num += s[i].str()
+    i++
+  }
+  return num.int(), s.substr(i, s.len)
+}
+
+fn consume_operator(s string) ?(string, string) {
+  op := s[0].str()
+  if op in '+-' {
+    return op, s.substr(1, s.len)
+  }
+  return error('invalid operator: $op')
 }
 
